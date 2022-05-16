0ad19c869d973881b15ff5e50721c2ccde72806f
Onur Ustunel <ustunel.onur@gmail.com>
2022-05-16 19:26:40 +0300

    Storyboard is removed

--- OpenCharge/OpenCharge/View/ChargeListViewController.swift
+++ OpenCharge/OpenCharge/View/ChargeListViewController.swift
@@ -12,6 +12,7 @@ class ChargeListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
 }
