292033b80fbbad2850883e29b15b62940a7f3c73
Onur Ustunel <ustunel.onur@gmail.com>
2022-05-16 19:16:46 +0300

    Core Folders are created

--- OpenCharge/OpenCharge/View/ChargeListViewController.swift
+++ OpenCharge/OpenCharge/View/ChargeListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ChargeListViewController.swift
 //  OpenCharge
 //
 //  Created by Onur Ustunel on 16.05.2022.
@@ -14,6 +14,4 @@ class ChargeListViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
 }
-
